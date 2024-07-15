59a18f3205f9d8e079ee97331d3ca66ac07aa977
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2024-07-14 21:27:56 -0300

    :white_check_mark: XDrive: add tested and approved XDrive subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/XDriveConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.robot.constants;
+
+public class XDriveConstants {
+    public static final String MOTOR_FRONT_RIGHT = "frontRight";
+    public static final String MOTOR_BACK_RIGHT = "backRight";
+    public static final String MOTOR_FRONT_LEFT = "frontLeft";
+    public static final String MOTOR_BACK_LEFT = "backLeft";
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/XDrive.java
@@ -1,5 +1,156 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
-public class XDrive {
-    
+import static org.firstinspires.ftc.teamcode.robot.constants.XDriveConstants.*;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.core.lib.gamepad.GamepadManager;
+import org.firstinspires.ftc.teamcode.core.lib.gamepad.SmartGamepad;
+import org.firstinspires.ftc.teamcode.core.lib.interfaces.Subsystem;
+
+public class XDrive implements Subsystem {
+
+    private  Telemetry telemetry;
+    private static XDrive instance;
+    private double reset_angle = 0;
+    private DcMotor front_left = null;
+    private DcMotor back_left = null;
+    private DcMotor back_right = null;
+    private DcMotor front_right = null;
+    private BNO055IMU imu;
+
+    private XDrive() {
+    }
+
+    @Override
+    public void initialize(HardwareMap hardwareMap, Telemetry telemetry) {
+        front_left = hardwareMap.dcMotor.get(MOTOR_FRONT_LEFT);
+        back_left = hardwareMap.dcMotor.get(MOTOR_BACK_LEFT);
+        back_right = hardwareMap.dcMotor.get(MOTOR_BACK_RIGHT);
+        front_right = hardwareMap.dcMotor.get(MOTOR_FRONT_RIGHT);
+
+        front_left.setDirection(DcMotor.Direction.REVERSE);
+        back_left.setDirection(DcMotor.Direction.REVERSE);
+        front_right.setDirection(DcMotor.Direction.FORWARD);
+        back_right.setDirection(DcMotor.Direction.FORWARD);
+
+        front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        this.telemetry = telemetry;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+        imu.initialize(parameters);
+
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void execute(GamepadManager gamepadManager) {
+        drive(gamepadManager.getDriver());
+        resetAngle(gamepadManager.getDriver());
+    }
+
+    public void drive(SmartGamepad driver) {
+        double Protate = driver.getRightStickX()/4;
+        double stick_x = driver.getLeftStickX() * Math.sqrt(Math.pow(1-Math.abs(Protate), 2)/2);
+        double stick_y = driver.getLeftStickY() * Math.sqrt(Math.pow(1-Math.abs(Protate), 2)/2);
+        double theta = 0;
+        double Px = 0;
+        double Py = 0;
+
+        double gyroAngle = getHeading() * Math.PI / 180;
+        if (gyroAngle <= 0) {
+            gyroAngle = gyroAngle + (Math.PI / 2);
+        } else if (0 < gyroAngle && gyroAngle < Math.PI / 2) {
+            gyroAngle = gyroAngle + (Math.PI / 2);
+        } else if (Math.PI / 2 <= gyroAngle) {
+            gyroAngle = gyroAngle - (3 * Math.PI / 2);
+        }
+        gyroAngle = -1 * gyroAngle;
+
+        if(driver.isButtonRightBumper()){
+            gyroAngle = -Math.PI/2;
+        }
+
+        if(driver.isButtonDPadRight()){
+            stick_x = 0.5;
+        }
+        else if(driver.isButtonDPadLeft()){
+            stick_x = -0.5;
+        }
+        if(driver.isButtonDPadUp()){
+            stick_y = -0.5;
+        }
+        else if(driver.isButtonDPadDown()){
+            stick_y = 0.5;
+        }
+
+
+        //MOVEMENT
+        theta = Math.atan2(stick_y, stick_x) - gyroAngle - (Math.PI / 2);
+        Px = Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)) * (Math.sin(theta + Math.PI / 4));
+        Py = Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)) * (Math.sin(theta - Math.PI / 4));
+
+        telemetry.addData("Stick_X", stick_x);
+        telemetry.addData("Stick_Y", stick_y);
+        telemetry.addData("Magnitude",  Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)));
+        telemetry.addData("Front Left", Py - Protate);
+        telemetry.addData("Back Left", Px - Protate);
+        telemetry.addData("Back Right", Py + Protate);
+        telemetry.addData("Front Right", Px + Protate);
+
+        front_left.setPower(Py - Protate);
+        back_left.setPower(Px - Protate);
+        back_right.setPower(Py + Protate);
+        front_right.setPower(Px + Protate);
+    }
+
+    public void resetAngle(SmartGamepad driver){
+        if(driver.isButtonA()){
+            reset_angle = getHeading() + reset_angle;
+        }
+    }
+
+    public double getHeading(){
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double heading = angles.firstAngle;
+        if(heading < -180) {
+            heading = heading + 360;
+        }
+        else if(heading > 180){
+            heading = heading - 360;
+        }
+        heading = heading - reset_angle;
+        return heading;
+    }
+
+    public static synchronized XDrive getInstance() {
+        if (instance == null) {
+            instance = new XDrive();
+        }
+        return instance;
+    }
+
 }
