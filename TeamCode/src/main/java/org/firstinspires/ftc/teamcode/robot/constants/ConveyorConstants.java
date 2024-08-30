c87d3a9ae1f62954a36b90fc2806523743a69f11
Davi Feitosa <davifeitosassa@gmail.com>
2024-08-30 15:06:20 -0300

    :sparkles: feat(conveyor): create conveyor subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/ConveyorConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.robot.constants;
+
+public class ConveyorConstants {
+    public static final String BOX_EXPANSION_SERVO = "boxExpansionServo";
+    public static final String LEFT_OUTTAKE_SERVO = "leftouttakeServo";
+    public static final String RIGHT_OUTTAKE_SERVO = "rightouttakeServo";
+    public static final String OUTTAKE_SENSOR = "outtakeColorSensor";
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ConveyorSubsystem.java
@@ -0,0 +1,118 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+import static org.firstinspires.ftc.teamcode.robot.constants.ConveyorConstants.*;
+
+import org.firstinspires.ftc.teamcode.core.lib.gamepad.GamepadManager;
+import org.firstinspires.ftc.teamcode.core.lib.gamepad.SmartGamepad;
+import org.firstinspires.ftc.teamcode.core.lib.interfaces.Subsystem;
+
+public class ConveyorSubsystem implements Subsystem {
+    private static ConveyorSubsystem instance;
+
+    private Servo boxExpansionServo;
+    private CRServo leftouttakeServo;
+    private CRServo rightouttakeServo;
+    private ColorSensor outtakeSensor;
+
+    private SmartGamepad operator;
+    private Telemetry telemetry;
+
+    private double boxExpandedPosition = 0; //Placeholder
+    private double boxRetractedPosition = 0; //Placeholder
+
+    private int[] foodARGBColor = {0,0,0,0}; //Placeholder
+    private int[] energyARGBColor = {0,0,0,0}; //Placeholder
+    private int[] waterARGBColor = {0,0,0,0}; //Placeholder
+
+
+    @Override
+    public void initialize(HardwareMap hardwareMap, Telemetry telemetry) {
+        boxExpansionServo = hardwareMap.get(Servo.class, BOX_EXPANSION_SERVO);
+        leftouttakeServo = hardwareMap.get(CRServo.class, LEFT_OUTTAKE_SERVO);
+        rightouttakeServo = hardwareMap.get(CRServo.class, RIGHT_OUTTAKE_SERVO);
+        outtakeSensor = hardwareMap.get(ColorSensor.class, OUTTAKE_SENSOR);
+
+        boxExpansionServo.setDirection(Servo.Direction.FORWARD);
+        leftouttakeServo.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightouttakeServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        this.telemetry = telemetry;
+
+        telemetry.addData("Conveyor Subsystem", "Initialized");
+    }
+
+    @Override
+    public void start() {
+        telemetry.addData("Box Expansion", "Retracted");
+    }
+
+    @Override
+    public void execute(GamepadManager gamepadManager) {
+        operator = gamepadManager.getOperator();
+
+        if(operator.isButtonX()) {
+            expandBox(boxExpandedPosition);
+            telemetry.addData("Box Expansion", "Expanded");
+        }
+        if(operator.isButtonY()) {
+            expandBox(boxRetractedPosition);
+            telemetry.addData("Box Expansion", "Retracted");
+        }
+
+        controlOuttake(operator);
+
+        telemetry.addData("Outtake Game Piece", gamePieceColorChecker());
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    private void expandBox(double Position) {
+        boxExpansionServo.setPosition(Position);
+    }
+
+    private void controlOuttake(SmartGamepad operator) {
+        if (operator.isRightTriggerPressed()){
+            leftouttakeServo.setPower(operator.getRightTrigger());
+            rightouttakeServo.setPower(operator.getRightTrigger());
+        } else if(operator.isLeftTriggerPressed()) {
+            leftouttakeServo.setPower(-operator.getLeftTrigger());
+            rightouttakeServo.setPower(-operator.getLeftTrigger());
+        }
+    }
+
+    private String gamePieceColorChecker() {
+        if (outtakeSensor.red() > foodARGBColor[0]-30 && outtakeSensor.green() < foodARGBColor[0]+30
+        && outtakeSensor.green() > foodARGBColor[1]-30 && outtakeSensor.green() < foodARGBColor[1]+30
+        && outtakeSensor.blue() > foodARGBColor[2]-30 && outtakeSensor.green() < foodARGBColor[2]+30){
+            return "Food";
+        } else if (outtakeSensor.red() > energyARGBColor[0]-30 && outtakeSensor.green() < energyARGBColor[0]+30
+                && outtakeSensor.green() > energyARGBColor[1]-30 && outtakeSensor.green() < energyARGBColor[1]+30
+                && outtakeSensor.blue() > energyARGBColor[2]-30 && outtakeSensor.green() < energyARGBColor[2]+30){
+            return "Energy";
+        } else if (outtakeSensor.red() > waterARGBColor[0]-30 && outtakeSensor.green() < waterARGBColor[0]+30
+                && outtakeSensor.green() > waterARGBColor[1]-30 && outtakeSensor.green() < waterARGBColor[1]+30
+                && outtakeSensor.blue() > waterARGBColor[2]-30 && outtakeSensor.green() < waterARGBColor[2]+30){
+            return "Water";
+        } else {
+            return "Without GP";
+        }
+    }
+
+    public static synchronized ConveyorSubsystem getInstance() {
+        if (instance == null) {
+            instance = new ConveyorSubsystem();
+        }
+        return instance;
+    }
+}
