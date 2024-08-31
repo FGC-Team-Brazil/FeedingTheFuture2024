2f5fe4e1dfda3a92dd94bcebc740c5f974c91f8c
Davi Feitosa <davifeitosassa@gmail.com>
2024-08-31 15:55:53 -0300

    feat: add second motor to linear subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/LinearSlide.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
