26020519f120cfe98a467ec996eaf1d811800232
Davi Feitosa <davifeitosassa@gmail.com>
2024-09-21 16:05:29 -0300

    chore(Outtake): comment colorSensor code for tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Outtake.java
@@ -16,17 +16,17 @@ public class Outtake  implements Subsystem {
     private Telemetry telemetry;
     private Servo servoRight;
     private Servo servoLeft;
-    private ColorSensor colorSensor;
+    /*private ColorSensor colorSensor;*/
 
     private Outtake() {}
     @Override
     public void initialize(HardwareMap hardwareMap, Telemetry telemetry) {
         this.servoRight = hardwareMap.get(Servo.class, SERVO_RIGHT);
         this.servoLeft = hardwareMap.get(Servo.class, SERVO_LEFT);
-        this.colorSensor = hardwareMap.get(ColorSensor.class, COLOR_SENSOR);
+        /*this.colorSensor = hardwareMap.get(ColorSensor.class, COLOR_SENSOR);*/
         this.telemetry = telemetry;
 
-        this.colorSensor.enableLed(true);
+        /*this.colorSensor.enableLed(true);*/
 
         this.telemetry.addData("Outtake Subsystem", "Initialized");
     }
@@ -57,15 +57,15 @@ public class Outtake  implements Subsystem {
                     servoLeft.setPosition(1);
                 });
 
-        if(colorSensor.green() > 100) {
+/*        if(colorSensor.green() > 100) {
             operator.ledSetColorContinuous(SmartGamepad.Color.RED);
         } else if (colorSensor.blue() > 100) {
             operator.ledSetColorContinuous(SmartGamepad.Color.BLUE);
         } else {
             operator.ledSetColorContinuous(SmartGamepad.Color.WHITE);
-        }
+        }*/
 
-        this.telemetry.addData("Outtake Color Sensor", "Red: %d, Green: %d, Blue: %d", colorSensor.red(), colorSensor.green(), colorSensor.blue());
+        /*this.telemetry.addData("Outtake Color Sensor", "Red: %d, Green: %d, Blue: %d", colorSensor.red(), colorSensor.green(), colorSensor.blue());*/
 
     }
 
